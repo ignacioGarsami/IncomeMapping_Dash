b9e37619efbde973390c88b51ba35a5f8b7cfbe8
unknown <ig231996@gmail.com>
2020-04-01 15:03:14 +0200

    added graph

--- app.py
+++ app.py
@@ -148,43 +148,45 @@ app.layout = html.Div(
                     ],
                 ),
 # =============================================================================
-#                 html.Div(
-#                     id="graph-container",
-#                     children=[
-#                         html.P(id="chart-selector", children="Select chart:"),
-#                         dcc.Dropdown(
-#                             options=[
-#                                 {
-#                                     "label": "Box plot of mean income by State",
-#                                     "value": "boxplot",
-#                                 },
-#                                 {
-#                                     "label": "Bar plot of mean income by State",
-#                                     "value": "barplot",
-#                                 }
-#                             ],
-#                             value="barplot",
-#                             id="chart-dropdown",
-#                         ),
-#                         dcc.Graph(
-#                             id="selected-data",
-#                             figure=dict(
-#                                 data=[dict(x=0, y=0)],
-#                                 layout=dict(
-#                                     paper_bgcolor="#F4F4F8",
-#                                     plot_bgcolor="#F4F4F8",
-#                                     autofill=True,
-#                                     margin=dict(t=75, r=50, b=100, l=50),
-#                                 ),
-#                             ),
-#                         ),
-#                     ],
-#                 ),
+                 html.Div(
+                     id="graph-container",
+                     children=[
+                         html.P(id="chart-selector", children="Select chart:"),
+                         dcc.Dropdown(
+                             options=[
+                                 {
+                                     "label": "Box plot of mean income by State",
+                                     "value": "boxplot",
+                                 },
+                                 {
+                                     "label": "Bar plot of mean income by State",
+                                     "value": "barplot",
+                                 }
+                             ],
+                             value="barplot",
+                             id="chart-dropdown",
+                         ),
+                         dcc.Graph(
+                             id="selected-data",
+                             figure=dict(
+                                 data=[dict(x=0, y=0)],
+                                 layout=dict(
+                                     paper_bgcolor="#F4F4F8",
+                                     plot_bgcolor="#F4F4F8",
+                                     autofill=True,
+                                     margin=dict(t=75, r=50, b=100, l=50),
+                                 ),
+                             ),
+                         ),
+                     ],
+                 ),
 # =============================================================================
             ],
         ),
     ],
 )
+                                        
+
                         
 
 @app.callback(
@@ -192,18 +194,19 @@ app.layout = html.Div(
     [Input("state-select", "value")],
     [State("county-choropleth", "figure")],
 )
-def display_map(income, figure):
+def display_map(value, figure):
     cm = dict(zip(bins, scale))
-
+    dff = data_map[data_map["State_Name"] == value]
+    
     data = [
         dict(
-            lat=data_map["Lat"],
-            lon=data_map["Lon"],
+            lat=dff["Lat"],
+            lon=dff["Lon"],
             hovertext = [["State: {} <br>County: {} <br>City: {} <br>Mean income: {}".format(i,j,v,w)]
-                                for i,j,v,w in zip(data_map['State_Name'], data_map['County'], data_map['City'], data_map['Mean'])],
+                                for i,j,v,w in zip(dff['State_Name'], dff['County'], dff['City'], dff['Mean'])],
             type="scattermapbox",
             hoverinfo="text",
-            marker=dict(size=5, color="white", opacity=0),
+            marker=dict(size=5, color=scale, opacity=5),
         )
     ]
 
@@ -259,153 +262,49 @@ def display_map(income, figure):
         dragmode="lasso",
     )
 
-#    base_url = "https://raw.githubusercontent.com/jackparmer/mapbox-counties/master/"
-#    for bin in bins:
-#        geo_layer = dict(
-#            sourcetype="geojson",
-#            source=base_url + str(income) + "/" + bin + ".geojson",
-#            type="fill",
-#            color=cm[bin],
-#            opacity=DEFAULT_OPACITY,
-#            # CHANGE THIS
-#            fill=dict(outlinecolor="#afafaf"),
-#        )
-#        layout["mapbox"]["layers"].append(geo_layer)
-
     fig = dict(data=data, layout=layout)
     return fig
 
 
-#
-#@app.callback(
-#    Output("selected-data", "figure"),
-#    [
-#        Input("county-choropleth", "selectedData"),
-#        Input("chart-dropdown", "value"),
-#        Input("years-slider", "value"),
-#    ],
-#)
-#def display_selected_data(selectedData, chart_dropdown, state):
-#    if selectedData is None:
-#        return dict(
-#            data=[dict(x=0, y=0)],
-#            layout=dict(
-#                title="Click-drag on the map to select counties",
-#                paper_bgcolor="#1f2630",
-#                plot_bgcolor="#1f2630",
-#                font=dict(color="#2cfec1"),
-#                margin=dict(t=75, r=50, b=100, l=75),
-#            ),
-#        )
-#    pts = selectedData["points"]
-#    fips = [str(pt["text"].split("<br>")[-1]) for pt in pts]
-#    for i in range(len(fips)):
-#        if len(fips[i]) == 4:
-#            fips[i] = "0" + fips[i]
-#    dff = data[data["County"].isin(fips)]
-#    dff = dff.sort_values("Mean")
-#
-#    if chart_dropdown != "death_rate_all_time":
-#        title = "Absolute deaths per county, <b>1999-2016</b>"
-#        AGGREGATE_BY = "Deaths"
-#        if "show_absolute_deaths_single_year" == chart_dropdown:
-#            dff = dff[dff.Year == year]
-#            title = "Absolute deaths per county, <b>{0}</b>".format(year)
-#        elif "show_death_rate_single_year" == chart_dropdown:
-#            dff = dff[dff.Year == year]
-#            title = "Age-adjusted death rate per county, <b>{0}</b>".format(year)
-#            AGGREGATE_BY = "Age Adjusted Rate"
-#
-#        dff[AGGREGATE_BY] = pd.to_numeric(dff[AGGREGATE_BY], errors="coerce")
-#        deaths_or_rate_by_fips = dff.groupby("County")[AGGREGATE_BY].sum()
-#        deaths_or_rate_by_fips = deaths_or_rate_by_fips.sort_values()
-#        # Only look at non-zero rows:
-#        deaths_or_rate_by_fips = deaths_or_rate_by_fips[deaths_or_rate_by_fips > 0]
-#        fig = deaths_or_rate_by_fips.iplot(
-#            kind="bar", y=AGGREGATE_BY, title=title, asFigure=True
-#        )
-#
-#        fig_layout = fig["layout"]
-#        fig_data = fig["data"]
-#
-#        fig_data[0]["text"] = deaths_or_rate_by_fips.values.tolist()
-#        fig_data[0]["marker"]["color"] = "#2cfec1"
-#        fig_data[0]["marker"]["opacity"] = 1
-#        fig_data[0]["marker"]["line"]["width"] = 0
-#        fig_data[0]["textposition"] = "outside"
-#        fig_layout["paper_bgcolor"] = "#1f2630"
-#        fig_layout["plot_bgcolor"] = "#1f2630"
-#        fig_layout["font"]["color"] = "#2cfec1"
-#        fig_layout["title"]["font"]["color"] = "#2cfec1"
-#        fig_layout["xaxis"]["tickfont"]["color"] = "#2cfec1"
-#        fig_layout["yaxis"]["tickfont"]["color"] = "#2cfec1"
-#        fig_layout["xaxis"]["gridcolor"] = "#5b5b5b"
-#        fig_layout["yaxis"]["gridcolor"] = "#5b5b5b"
-#        fig_layout["margin"]["t"] = 75
-#        fig_layout["margin"]["r"] = 50
-#        fig_layout["margin"]["b"] = 100
-#        fig_layout["margin"]["l"] = 50
-#
-#        return fig
-#
-#    fig = dff.iplot(
-#        kind="area",
-#        x="Year",
-#        y="Age Adjusted Rate",
-#        text="County",
-#        categories="County",
-#        colors=[
-#            "#1b9e77",
-#            "#d95f02",
-#            "#7570b3",
-#            "#e7298a",
-#            "#66a61e",
-#            "#e6ab02",
-#            "#a6761d",
-#            "#666666",
-#            "#1b9e77",
-#        ],
-#        vline=[year],
-#        asFigure=True,
-#    )
-#
-#    for i, trace in enumerate(fig["data"]):
-#        trace["mode"] = "lines+markers"
-#        trace["marker"]["size"] = 4
-#        trace["marker"]["line"]["width"] = 1
-#        trace["type"] = "scatter"
-#        for prop in trace:
-#            fig["data"][i][prop] = trace[prop]
-#
-#    # Only show first 500 lines
-#    fig["data"] = fig["data"][0:500]
-#
-#    fig_layout = fig["layout"]
-#
-#    # See plot.ly/python/reference
-#    fig_layout["yaxis"]["title"] = "Age-adjusted death rate per county per year"
-#    fig_layout["xaxis"]["title"] = ""
-#    fig_layout["yaxis"]["fixedrange"] = True
-#    fig_layout["xaxis"]["fixedrange"] = False
-#    fig_layout["hovermode"] = "closest"
-#    fig_layout["title"] = "<b>{0}</b> counties selected".format(len(fips))
-#    fig_layout["legend"] = dict(orientation="v")
-#    fig_layout["autosize"] = True
-#    fig_layout["paper_bgcolor"] = "#1f2630"
-#    fig_layout["plot_bgcolor"] = "#1f2630"
-#    fig_layout["font"]["color"] = "#2cfec1"
-#    fig_layout["xaxis"]["tickfont"]["color"] = "#2cfec1"
-#    fig_layout["yaxis"]["tickfont"]["color"] = "#2cfec1"
-#    fig_layout["xaxis"]["gridcolor"] = "#5b5b5b"
-#    fig_layout["yaxis"]["gridcolor"] = "#5b5b5b"
-#
-#    if len(fips) > 500:
-#        fig["layout"][
-#            "title"
-#        ] = "Age-adjusted death rate per county per year <br>(only 1st 500 shown)"
-#
-#    return fig
 
+@app.callback(
+    Output("selected-data", "figure"),
+    [
+        Input("chart-dropdown", "value"),
+        Input("state-select", "value"),
+    ],
+)
+def display_selected_data(chart_dropdown, state):
+
+    dff = data_map[data_map["State_Name"] == state]
+    dff = dff.sort_values("Mean")
+
+    title = "Bar plot of mean income per county in selected state"
+    
+    fig = go.Figure([go.Bar(x=dff['County'], y=['Mean'])])
+
+    fig_layout = fig["layout"]
+    fig_data = fig["data"]
+
+    fig_data[0]["text"] = dff.values.tolist()
+    fig_data[0]["marker"]["color"] = "#2cfec1"
+    fig_data[0]["marker"]["opacity"] = 1
+    fig_data[0]["marker"]["line"]["width"] = 0
+    fig_data[0]["textposition"] = "outside"
+    fig_layout["paper_bgcolor"] = "#1f2630"
+    fig_layout["plot_bgcolor"] = "#1f2630"
+    fig_layout["font"]["color"] = "#2cfec1"
+    fig_layout["title"]["font"]["color"] = "#2cfec1"
+    fig_layout["xaxis"]["tickfont"]["color"] = "#2cfec1"
+    fig_layout["yaxis"]["tickfont"]["color"] = "#2cfec1"
+    fig_layout["xaxis"]["gridcolor"] = "#5b5b5b"
+    fig_layout["yaxis"]["gridcolor"] = "#5b5b5b"
+    fig_layout["margin"]["t"] = 75
+    fig_layout["margin"]["r"] = 50
+    fig_layout["margin"]["b"] = 100
+    fig_layout["margin"]["l"] = 50
+
+    return fig
 
 
 if __name__ == "__main__":
